9a90e8ef03f0d337d7eca02f75d61ab491f8b62f
Md. Harun-Or-Rashid <sajjadcse161@gmail.com>
2021-07-27 10:53:19 +0600

    Add files via upload

---
+++ Bidirectional Visitor Counter with Automatic Light Control using Arduino/Bidirectional_Visitor_Counter_with_Automatic_Light_Control_usin.ino
@@ -0,0 +1,70 @@
+//Visitors Counter and Automatic Room Light
+
+#include<LiquidCrystal.h>
+int Contrast = 60;
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+#define in 14
+#define out 19
+#define relay 1
+
+int count=0;
+
+void IN()
+{
+    count++;
+    lcd.clear();
+    lcd.print("Person In Room:");
+    lcd.setCursor(0,1);
+    lcd.print(count);
+    delay(1000);
+}
+
+void OUT()
+{
+  count--;
+    lcd.clear();
+    lcd.print("Person In Room:");
+    lcd.setCursor(0,1);
+    lcd.print(count);
+    delay(1000);
+}
+
+void setup()
+{
+  analogWrite(6, Contrast);
+  lcd.begin(16,2);
+  lcd.print("Visitor Counter");
+  delay(2000);
+  pinMode(in, INPUT);
+  pinMode(out, INPUT);
+  pinMode(relay, OUTPUT);
+  lcd.clear();
+  lcd.print("Person In Room:");
+  lcd.setCursor(0,1);
+  lcd.print(count);
+}
+
+void loop()
+{  
+  
+  if(digitalRead(in))
+  IN();
+  if(digitalRead(out))
+  OUT();
+  
+  if(count<=0)
+  {
+    lcd.clear();
+    digitalWrite(relay, LOW);
+    lcd.clear();
+    lcd.print("Nobody In Room");
+    lcd.setCursor(0,1);
+    lcd.print("Light Is Off");
+    delay(200);
+  }
+  
+  else
+    digitalWrite(relay, HIGH);
+  
+}
